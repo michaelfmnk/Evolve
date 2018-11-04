a9f81680055f4c52183a457de9e8ff87c3cc4a70
lika_sh <lika.shugurova@gmail.com>
2018-11-04 15:38:31 +0200

    nothing

--- api/src/main/java/com/dreamteam/api/dtos/CardDto.java
+++ api/src/main/java/com/dreamteam/api/dtos/CardDto.java
@@ -1,4 +1,24 @@
 package com.dreamteam.api.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CardDto {
+    private Integer id;
+    @NotBlank
+    private String title;
+    private String content;
+    private LocalDateTime createdTime;
+    private Integer columnId;
 }
